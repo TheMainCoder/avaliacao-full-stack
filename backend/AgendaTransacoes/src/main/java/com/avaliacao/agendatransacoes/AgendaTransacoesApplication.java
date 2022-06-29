6ce46a6016d66f9e4971029feee56de709ff7845
TheMainCoder <ailtonfranciscodasilva@hotmail.com>
2022-06-29 15:51:10 -0300

    Initial import projeto backend

---
+++ backend/AgendaTransacoes/src/main/java/com/avaliacao/agendatransacoes/AgendaTransacoesApplication.java
@@ -0,0 +1,13 @@
+package com.avaliacao.agendatransacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaTransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaTransacoesApplication.class, args);
+	}
+
+}
---
+++ backend/AgendaTransacoes/src/test/java/com/avaliacao/agendatransacoes/AgendaTransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avaliacao.agendatransacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaTransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
